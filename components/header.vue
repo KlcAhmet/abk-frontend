1b0feec085a27b63ef3602f7222484f1865561c6
Ahmet Batuhan Kılıç <ahmetbatukilic@gmail.com>
2023-07-23 19:46:08 +0300

    continue header.vue for update

--- components/header.vue
+++ components/header.vue
@@ -3,6 +3,7 @@
 const langSelectVisible = ref(true);
 const { locale, t } = useI18n();
 const route = useRoute();
+const routeName = computed(() => t(route.name as string));
 
 function changeLangVisible(): void {
   langSelectVisible.value = !langSelectVisible.value;
@@ -14,39 +15,49 @@ function changeLangVisible(): void {
 </script>
 
 <template>
-  <header class='relative h-24 flex items-center px-5 border-b-2 border-sky-200/100'>
-    <div class='absolute inset-x-1/2 transform -translate-x-1/2 w-16 h-16 -ml-5'>
-      <figure>
-        <img src='@/assets/img/astronaut-icon.svg' alt='logo' />
-      </figure>
-    </div>
-    <nav class='flex-none ml-auto'>
-      <ul class='flex flex-row flex-nowrap items-center space-x-4 text-white text-xl font-bold'>
-        <li>
-          <NuxtLink class='hover:text-sky-500' :class='route.name' to='/'>{{ t('home') }}</NuxtLink>
-        </li>
-        <li>
-          <NuxtLink class='hover:text-sky-500' to='/about'>{{ t('about') }}</NuxtLink>
-        </li>
-        <li>
-          <NuxtLink class='hover:text-sky-500' to='/career'>{{ t('career') }}</NuxtLink>
-        </li>
-        <li>
-          <NuxtLink class='hover:text-sky-500' to='/contributions'>{{ t('contributions') }}</NuxtLink>
-        </li>
-      </ul>
-    </nav>
-    <div class='ml-4 flex'>
-      <button v-if='langSelectVisible' type='button' class='bg-sky-500 hover:bg-sky-700 rounded-full'
-              @click='changeLangVisible'>
-        <img class='w-8' src='@/assets/img/world.svg' alt='world img' />
-      </button>
-      <select v-else v-model='locale' @change='changeLangVisible'>
-        <option value='tr'>TR</option>
-        <option value='en'>EN</option>
-      </select>
-    </div>
-  </header>
+  <ClientOnly>
+    <header class='lg:hidden p-3'>
+      <div class='inline-block'>
+        <div class='p-1 bg-sky-300 rounded flex items-center flex-nowrap'>
+          <img class='w-8' src='@/assets/img/menu-nav.svg' alt='menu logo'>
+          <h6 class='ml-2'>{{ routeName }}</h6>
+        </div>
+      </div>
+    </header>
+    <header class='hidden relative h-24 lg:flex items-center px-5 border-b-2 border-sky-200/100'>
+      <div class='absolute inset-x-1/2 transform -translate-x-1/2 w-16 h-16 -ml-5'>
+        <figure>
+          <img src='@/assets/img/astronaut-icon.svg' alt='logo' />
+        </figure>
+      </div>
+      <nav class='flex-none ml-auto'>
+        <ul class='flex flex-row flex-nowrap items-center space-x-4 text-white text-xl font-bold'>
+          <li>
+            <NuxtLink class='hover:text-sky-500' to='/'>{{ t('index') }}</NuxtLink>
+          </li>
+          <li>
+            <NuxtLink class='hover:text-sky-500' to='/about'>{{ t('about') }}</NuxtLink>
+          </li>
+          <li>
+            <NuxtLink class='hover:text-sky-500' to='/career'>{{ t('career') }}</NuxtLink>
+          </li>
+          <li>
+            <NuxtLink class='hover:text-sky-500' to='/contributions'>{{ t('contributions') }}</NuxtLink>
+          </li>
+        </ul>
+      </nav>
+      <div class='ml-4 flex'>
+        <button v-if='langSelectVisible' type='button' class='bg-sky-500 hover:bg-sky-700 rounded-full'
+                @click='changeLangVisible'>
+          <img class='w-8' src='@/assets/img/world.svg' alt='world img' />
+        </button>
+        <select v-else v-model='locale' @change='changeLangVisible'>
+          <option value='tr'>TR</option>
+          <option value='en'>EN</option>
+        </select>
+      </div>
+    </header>
+  </ClientOnly>
 </template>
 
 <style scoped lang='sass'>
@@ -56,12 +67,12 @@ function changeLangVisible(): void {
 
 <i18n lang='yaml'>
 tr:
-  home: "Anasayfa"
+  index: "Anasayfa"
   about: "Hakkımda"
   career: "Kariyer"
   contributions: "Katkılar"
 en:
-  home: "Home"
+  index: "Home"
   about: "About"
   career: "Career"
   contributions: "Contributions"
