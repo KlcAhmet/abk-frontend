c6e92cf1271a36701114e92206f0beef834ecff9
Ahmet Batuhan Kılıç <ahmetbatukilic@gmail.com>
2023-07-23 20:42:40 +0300

    update header.vue for beauty

--- components/header.vue
+++ components/header.vue
@@ -22,6 +22,7 @@ const changeNavVisible = (val?: boolean): void => {
 </script>
 
 <template>
+  <!-- mobile -->
   <header class='lg:hidden p-3'>
     <div class='w-48 p-1 bg-sky-900 rounded'>
       <div class='flex flex-nowrap justify-between'>
@@ -64,37 +65,40 @@ const changeNavVisible = (val?: boolean): void => {
       </div>
     </div>
   </header>
+  <!-- desktop -->
   <header class='hidden relative h-24 lg:flex items-center px-5 border-b-2 border-sky-200/100'>
     <div class='absolute inset-x-1/2 transform -translate-x-1/2 w-16 h-16 -ml-5'>
       <figure>
         <img src='@/assets/img/astronaut-icon.svg' alt='logo' />
       </figure>
     </div>
-    <nav class='flex-none ml-auto'>
-      <ul class='flex flex-row flex-nowrap items-center space-x-4 text-white text-xl font-bold'>
-        <li>
-          <NuxtLink class='hover:text-sky-500' to='/'>{{ t('index') }}</NuxtLink>
-        </li>
-        <li>
-          <NuxtLink class='hover:text-sky-500' to='/about'>{{ t('about') }}</NuxtLink>
-        </li>
-        <li>
-          <NuxtLink class='hover:text-sky-500' to='/career'>{{ t('career') }}</NuxtLink>
-        </li>
-        <li>
-          <NuxtLink class='hover:text-sky-500' to='/contributions'>{{ t('contributions') }}</NuxtLink>
-        </li>
-      </ul>
-    </nav>
-    <div class='ml-4 flex'>
-      <button v-if='langSelectVisible' type='button' class='bg-sky-500 hover:bg-sky-700 rounded-full'
-              @click='changeLangVisible'>
-        <img class='w-8' src='@/assets/img/world.svg' alt='world img' />
-      </button>
-      <select v-else v-model='locale' @change='changeLangVisible'>
-        <option value='tr'>TR</option>
-        <option value='en'>EN</option>
-      </select>
+    <div class='ml-auto flex flex-nowrap py-3 px-8 bg-sky-700 rounded'>
+      <nav>
+        <ul class='flex flex-row flex-nowrap items-center space-x-4 text-white text-xl font-bold'>
+          <li>
+            <NuxtLink class='hover:text-sky-500' to='/'>{{ t('index') }}</NuxtLink>
+          </li>
+          <li>
+            <NuxtLink class='hover:text-sky-500' to='/about'>{{ t('about') }}</NuxtLink>
+          </li>
+          <li>
+            <NuxtLink class='hover:text-sky-500' to='/career'>{{ t('career') }}</NuxtLink>
+          </li>
+          <li>
+            <NuxtLink class='hover:text-sky-500' to='/contributions'>{{ t('contributions') }}</NuxtLink>
+          </li>
+        </ul>
+      </nav>
+      <div class='ml-4 flex'>
+        <button v-if='langSelectVisible' type='button'
+                @click='changeLangVisible'>
+          <img class='w-8' src='@/assets/img/world.svg' alt='world img' />
+        </button>
+        <select v-else v-model='locale' @change='changeLangVisible'>
+          <option value='tr'>TR</option>
+          <option value='en'>EN</option>
+        </select>
+      </div>
     </div>
   </header>
 </template>
