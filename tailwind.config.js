c6ba3ed01660374a8cf7c54833a89db756ad35cf
Ahmet Batuhan Kılıç <ahmetbatukilic@gmail.com>
2023-07-23 04:37:04 +0300

    instal and setup config i18n

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,20 @@ module.exports = {
   content: [],
   theme: {
     extend: {},
+    colors: {
+      transparent: 'transparent',
+      white: '#ffffff',
+      sky: {
+        200: '#bae6fd',
+        300: '#7dd3fc',
+        400: '#38bdf8',
+        500: '#0ea5e9',
+        600: '#0284c7',
+        700: '#0369a1',
+        800: '#075985',
+        900: '#0c4a6e',
+      },
+    },
   },
   plugins: [],
 };
