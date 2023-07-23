22f52b80dc30d6d478858f7c92654b7eacc9c61d
Ahmet Batuhan Kılıç <ahmetbatukilic@gmail.com>
2023-07-23 21:43:14 +0300

    update responsive

--- layouts/homeLayout.vue
+++ layouts/homeLayout.vue
@@ -1,12 +1,15 @@
-<script lang='ts'></script>
+<script lang='ts'>
+</script>
 
 <template>
   <div class='homeLayout h-screen bg-no-repeat bg-cover'>
-    <Header />
-    <main>
-      <slot />
-    </main>
-    <Footer />
+    <div class='max-w-[1800px] mx-auto'>
+      <Header />
+      <main class='h-screen'>
+        <slot />
+      </main>
+      <!--    <Footer />-->
+    </div>
   </div>
 </template>
 
