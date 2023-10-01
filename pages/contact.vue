ff6273db7a156e0c4b89c5e3ca428060cc43dc95
Ahmet Batuhan Kılıç <ahmetbatukilic@gmail.com>
2023-10-01 03:36:15 +0300

    Add validation,i18n and continue contact.vue

--- pages/contact.vue
+++ pages/contact.vue
@@ -2,67 +2,54 @@
 import type { Ref } from 'vue';
 import { ref } from 'vue';
 import { ITicket, ITicketResponse } from '~/server/dbModels';
-import * as yup from 'yup';
+import { createTicketValidate, IValidationErrors } from '~/server/validations';
 
 definePageMeta({
   layout: 'home-layout',
 });
 
-// const { t } = useI18n();
-const ticket: Ref<ITicket> = ref({
-  name: 'test-name-1',
-  mail: 'test-mail-1@gmail.com',
-  message: 'Bugün hava harika ve güneşli. Sabah yürüyüşü yaparken kuşların şarkıları beni mutlu ediyor. Akşam yemeği için arkadaşlarımla buluşacağım ve uzun zamandır görmediğimiz için çok heyecanlıyız. Gelecek hafta sonu ise doğa tatili yapmaya karar verdik. Ormanın derinliklerinde kamp kuracağız ve ateş başında hikayeler anlatacağız. Ayrıca yeni bir kitap okuma listesi oluşturdum ve şu anda bir romanı bitirmeye çalışıyorum. Hayat dolu ve heyecan verici!',
+const { t } = useI18n();
+const ticket: Ref<ITicket> = ref({ // TODO: remove default values
+  name: '',
+  mail: '',
+  message: 'Bugün hava harika ve güneşli. Sabah yürüyüşü yaparken kuşların şarkıları beni mutlu ediyor. Akşam yemeği için arkadaşlarımla buluşacağım ve uzun zamandır görmediğimiz için çok heyecanlıyız. Gelecek hafta sonu ise doğa tatili yapmaya karar verdik. Ormanın derinliklerinde kamp kuracağız ve ateş başında hikayeler anlatacağız. Ayrıca yeni bir kitap okuma listesi oluşturdum ve şu anda bir romanı bitirmeye çalışıyorum. Hayat dolu ve heyecan verici!vvvvBugün hava harika ve güneşli. Sabah yürüyüşü yaparken kuşların şarkıları beni mutlu ediyor. Akşam yemeği için arkadaşlarımla buluşacağım ve uzun zamandır görmediğimiz için çok heyecanlıyız. Gelecek hafta sonu ise doğa tatili yapmaya karar verdik. Ormanın derinliklerinde kamp kuracağız ve ateş başında hikayeler anlatacağız. Ayrıca yeni bir kitap okuma listesi oluşturdum ve şu anda bir romanı bitirmeye çalışıyorum. Hayat dolu ve heyecan verici! heyecan verici! heyecan verici! heyecan verici! heyecan verici! heyecan verici!' +
+    ' heyecan verici! heyecan b',
 });
-
-const validationErrors: {
-  value?: any
-  nameRequired?: boolean
-  nameMin?: boolean
-  nameMax?: boolean
-  mailInvalid?: boolean
-  mailRequired?: boolean
-  messageRequired?: boolean
-} = ref({
+const validationErrors: IValidationErrors = ref({
   nameRequired: false,
-  nameMin: false,
   nameMax: false,
   mailInvalid: false,
+  mailMax: false,
   mailRequired: false,
   messageRequired: false,
+  messageMax: false,
 });
+const validationReset: IValidationErrors = validationErrors.value;
+const isTicketPending: Ref<boolean> = ref(false);
 
-let ticketSchema = yup.object({
-  name: yup.string().required('nameRequired').min(3, 'nameMin').max(20, 'nameMax'),
-  mail: yup.string().email('mailInvalid').required('mailRequired'),
-  message: yup.string().required('messageRequired'),
-});
 const sendMail = async () => {
-  let isValid = false;
+  isTicketPending.value = true;
   try {
-    await ticketSchema.validate(ticket.value, { abortEarly: false }).then(() => {
-      isValid = true;
-      validationErrors.value = {};
-    }).catch((err) => {
-      isValid = false;
-      err.errors.forEach((key: any) => {
-        validationErrors.value[key] = true;
-      });
-    });
+    const validation = await createTicketValidate(ticket.value);
 
-    if (isValid) {
+    if (validation.isValid) {
       const res: ITicketResponse = await $fetch('/api/ticket', {
         method: 'POST',
         body: ticket.value,
       });
       if (res.statusCode === 200) {
-        console.log('res >>>', res);
+        validationErrors.value = validationReset;
+        // TODO: show success message
       } else {
-        console.error('res >>>', res);
+        console.error('res-err >>>', res);
+        // TODO: show error message
       }
+    } else {
+      validationErrors.value = validation.validationErrors;
     }
   } catch (error) {
-    console.log(error);
+  } finally {
+    isTicketPending.value = false;
   }
 };
 </script>
@@ -70,17 +57,29 @@ const sendMail = async () => {
 <template>
   <div class='h-full flex items-center'>
     <div>
-      <form style='background-color: cornflowerblue' class='flex flex-col p-3 w-[329px]'>
+      <form style='background-color: cornflowerblue' class='flex flex-col p-3 w-[375px]'>
         <label>Name:</label>
         <input type='text' v-model.trim='ticket.name'>
-        <p v-if='validationErrors.nameRequired'>isim gerekli</p>
+        <div>
+          <p v-if='validationErrors.nameRequired'>{{ t('nameRequired') }}</p>
+          <p v-else-if='validationErrors.nameMax'>{{ t('nameMax') }}</p>
+        </div>
         <label>Mail:</label>
         <input type='text' v-model.trim='ticket.mail'>
-        <p v-if='validationErrors.mailRequired'>mail gerekli</p>
+        <div>
+          <p v-if='validationErrors.mailRequired'>{{ t('mailRequired') }}</p>
+          <p v-else-if='validationErrors.mailInvalid'>{{ t('mailInvalid') }}</p>
+          <p v-else-if='validationErrors.mailMax'>{{ t('mailMax') }}</p>
+        </div>
         <label>Message:</label>
         <textarea v-model.trim='ticket.message' />
-        <p v-if='validationErrors.messageRequired'>mesaj gerekli</p>
-        <button class='p-3 bg-white mt-5' type='button' @click='sendMail'>Send</button>
+        <div>
+          <p v-if='validationErrors.messageRequired'>{{ t('messageRequired') }}</p>
+          <p v-else-if='validationErrors.messageMax'>{{ t('messageMax') }}</p>
+        </div>
+        <button class='p-3 bg-white mt-5' type='button' :disabled='isTicketPending' @click='sendMail'>
+          <span>{{ t('send') }}</span>
+        </button>
       </form>
     </div>
 
@@ -88,13 +87,25 @@ const sendMail = async () => {
 </template>
 
 <style scoped>
-
 </style>
 
-<!--
 <i18n lang='yaml'>
 tr:
-  description: "Şu anda, Frontend Geliştirme alanında öncelikli olarak React, Vue ve Node.js teknolojileri üzerinde çalışıyorum. Kendimi, web programlama konusunda yüksek bilgiye sahip ve güçlü sosyal becerilere sahip biri olarak tanımlıyorum. Ayrıca, hayatımın tüm alanlarında yüksek hedefler belirliyorum, hedeflerime ulaşmak için çalışkan ve kararlı biriyim."
+  nameRequired: "İsim alanı zorunludur"
+  nameMax: "İsim alanı 100 karakteri geçemez."
+  mailInvalid: "Mail geçersiz"
+  mailMax: "Mail alanı 100 karakteri geçemez."
+  mailRequired: "Mail alanı zorunludur"
+  messageRequired: "Mesaj alanı zorunludur"
+  messageMax: "Mesaj alanı 1000 karakteri geçemez."
+  send: "Gönder"
 en:
-  description: "At present, I'm working on primarily in React, Vue and Node.js technologies in the Frontend Development. I describe myself as a person who has high knowledge about web programming and strong social skills. Besides, I set high goals in all fields of my life, i am a hardworker and persistent person on reaching my goals."
-</i18n>-->
+  nameRequired: "Name is required"
+  nameMax: "Name field must not exceed 100 characters."
+  mailInvalid: "Mail is invalid"
+  mailMax: "Mail field must not exceed 100 characters."
+  mailRequired: "Mail is required"
+  messageRequired: "Message is required"
+  messageMax: "Message field must not exceed 1000 characters."
+  send: "Send"
+</i18n>
--- server/api/ticket/index.post.ts
+++ server/api/ticket/index.post.ts
@@ -1,32 +1,35 @@
 import { defineEventHandler, getHeaders, readBody } from 'h3';
 import { ITicket, ITicketResponse, IUserInfo } from '~/server/dbModels';
 import { CollectUsersInfo } from '~/server/api/mixins/collect-user-info';
+import { createTicketValidate } from '~/server/validations';
 
 export default defineEventHandler(async (event) => {
   const API_URL: string | undefined = process.env.API_URL;
   const headers: IUserInfo = new CollectUsersInfo(getHeaders(event)).getUserRemoteInfo();
-  const { name, mail, message }: ITicket = await readBody<ITicket>(event);
+  const ticket: ITicket = await readBody<ITicket>(event);
 
-  console.log(name, mail, message);
   try {
-    if (![name, mail, message].every(Boolean)) {
+    const { isValid } = await createTicketValidate(ticket);
+
+    if (isValid) {
+      const response: ITicketResponse = await $fetch(`${API_URL}/ticket`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          ...headers,
+        },
+        body: JSON.stringify(ticket),
+      });
+
       return {
-        status: 400,
+        statusCode: response.statusCode,
+      };
+    } else {
+      return {
+        statusCode: 400,
       };
     }
 
-    const response: ITicketResponse = await $fetch(`${API_URL}/ticket`, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-        ...headers,
-      },
-      body: JSON.stringify({ name, mail, message }),
-    });
-
-    return {
-      statusCode: response.statusCode,
-    };
   } catch (err) {
     return {
       statusCode: 500,
